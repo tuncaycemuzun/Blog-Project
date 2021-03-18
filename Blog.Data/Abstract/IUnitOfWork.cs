e4c6398f67b67c23277cfe4fb725b60ffebe2dfd
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-19 00:03:37 +0300

    Created UnitOfWork Desing Patern

---
+++ Blog.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Abstract
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IArticleRepository Articles { get; }
+        ICategoryRepository Categories { get; }
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; } // _unitOfWork.Categories.AddAsync();
+        Task<int> SaveAsync();
+    }
+}
---
+++ Blog.Data/Concrete/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Data.Abstract;
+using Blog.Data.Concrete.EntityFramework.Contexts;
+using Blog.Data.Concrete.EntityFramework.Repositories;
+
+namespace Blog.Data.Concrete
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly BlogContext _context;
+        private EfArticleRepository _articleRepository;
+        private EfCategoryRepository _efCategoryRepository;
+        private EfCommentRepository _efCommentRepository;
+        private EfRoleRepository _efRoleRepository;
+        private EfUserRepository _efUserRepository;
+        public UnitOfWork(BlogContext context)
+        {
+            _context = context;
+        }
+
+        public IArticleRepository Articles => _articleRepository ?? new EfArticleRepository(_context);
+        public ICategoryRepository Categories => _efCategoryRepository ?? new EfCategoryRepository(_context);
+        public ICommentRepository Comments => _efCommentRepository ?? new EfCommentRepository(_context);
+        public IRoleRepository Roles => _efRoleRepository ?? new EfRoleRepository(_context);
+        public IUserRepository Users => _efUserRepository ?? new EfUserRepository(_context);
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+    }
+}
