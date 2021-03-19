e5babb459667ebff24af7bab82d9f60a928baa2a
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-19 18:02:11 +0300

    Create Result.cs

---
+++ Blog.Shared/Utilities/Results/Concrete/Result.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Shared.Utilities.Results.Abstract;
+using Blog.Shared.Utilities.Results.ComplexTypes;
+
+namespace Blog.Shared.Utilities.Results.Concrete
+{
+    public class Result : IResult
+    {
+        public Result(ResultStatus resultStatus)
+        {
+            ResultStatus = resultStatus;
+        }
+        public Result(ResultStatus resultStatus,string message)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+        }
+        public Result(ResultStatus resultStatus, string message,Exception e)
+        {
+            Exception = e;
+            ResultStatus = resultStatus;
+            Message = message;
+        }
+        public ResultStatus ResultStatus { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+    }
+}
