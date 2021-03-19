2f8f583f06d951e1ee260ebf9e9df8a5904e25e1
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-19 22:36:08 +0300

    Create CategoryController.cs

---
+++ Blog.MVC/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Services.Abstract;
+using Blog.Shared.Utilities.Results.ComplexTypes;
+
+namespace Blog.MVC.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [Area("Admin")]
+        public async Task<IActionResult> Index()
+        {
+            var result = await _categoryService.GetAll();
+            if (result.ResultStatus == ResultStatus.Success)
+            {
+                return View(result.Data);
+            }
+            return View();
+        }
+    }
+}
