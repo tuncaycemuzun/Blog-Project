e9e0ebfd9e0da7c01a80ca2a140fffb2c34dbe2e
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-19 14:49:05 +0300

    Update Category.cs

--- Blog.Entities/Concrete/Category.cs
+++ Blog.Entities/Concrete/Category.cs
@@ -11,7 +11,6 @@ namespace Blog.Entities.Concrete
     {
         public string Name { get; set; }
         public string Description { get; set; }
-
         public ICollection<Article> Articles { get; set; }
     }
 }
