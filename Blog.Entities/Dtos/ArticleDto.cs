9ee38b527f53fefc3d73b7cc8864e4a018428d24
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-19 19:43:02 +0300

    Update ArticleDto.cs

--- Blog.Entities/Dtos/ArticleDto.cs
+++ Blog.Entities/Dtos/ArticleDto.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Blog.Entities.Concrete;
+using Blog.Shared.Entities.Abstract;
+using Blog.Shared.Utilities.Results.ComplexTypes;
 
 namespace Blog.Entities.Dtos
 {
-    public class ArticleDto
+    public class ArticleDto : DtoGetBase
     {
+        public Article Article { get; set; }
     }
 }
