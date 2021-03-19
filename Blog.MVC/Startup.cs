7bfabac8549831fff88a44099032fe049462964b
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-19 21:31:29 +0300

    Update Startup.cs

--- Blog.MVC/Startup.cs
+++ Blog.MVC/Startup.cs
@@ -17,6 +17,7 @@ namespace Blog.MVC
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
             services.LoadMyServices();
         }
 
@@ -26,16 +27,20 @@ namespace Blog.MVC
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();//404 NotFound
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapAreaControllerRoute(
+                    name:"Admin",
+                    areaName:"Admin",
+                    pattern:"Admin/{controller=Home}/{action=Index}/{id?}"
+                );
+                endpoints.MapDefaultControllerRoute();//Home/Index
             });
         }
     }
