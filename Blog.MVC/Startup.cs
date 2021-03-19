88e115e1689f2073f21e47f9a11954a8e889e00e
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-19 23:22:02 +0300

    Update Startup.cs

--- Blog.MVC/Startup.cs
+++ Blog.MVC/Startup.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.Services.AutoMapper.Profiles;
 using Blog.Services.Extensions;
 
 namespace Blog.MVC
@@ -18,7 +19,7 @@ namespace Blog.MVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
-            services.AddAutoMapper(typeof(Startup));
+            services.AddAutoMapper(typeof(CategoryProfile),typeof(ArticleProfile));
             services.LoadMyServices();
         }
 
