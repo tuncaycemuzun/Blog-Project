8afe6375e3a7662aeb495fffd24e7c710f7d7941
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-19 18:17:13 +0300

    Create ICategoryService.cs

---
+++ Blog.Services/Abstract/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Blog.Entities.Concrete;
+using Blog.Entities.Dtos;
+using Blog.Shared.Utilities.Results.Abstract;
+
+namespace Blog.Services.Abstract
+{
+    public interface ICategoryService
+    {
+        Task<IDataResult<Category>> Get(int categoryId);
+        Task<IDataResult<IList<Category>>> GetAll();
+        Task<IResult> Add(CategoryAddDto categoryAddDto,string createdByName);
+        Task<IResult> Update(CategoryUpdateDto categoryUpdateDto,string modifiedByName);
+        Task<IResult> Delete(int categoryId);
+        Task<IResult> HardDelete(int categoryId);
+    }
+}
