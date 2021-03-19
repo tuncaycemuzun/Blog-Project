81982cf3e8ab198aed2dba8c762d1f04e1df9662
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-19 19:23:36 +0300

    Create IArticleService.cs

---
+++ Blog.Services/Abstract/IArticleService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Entities.Concrete;
+using Blog.Entities.Dtos;
+using Blog.Shared.Utilities.Results.Abstract;
+
+namespace Blog.Services.Abstract
+{
+    public interface IArticleService
+    {
+        Task<IDataResult<Article>> Get(int categoryId);
+        Task<IDataResult<IList<Article>>> GetAll();
+        Task<IDataResult<IList<Article>>> GetAllByNonDeleted();
+        Task<IResult> Add(ArticleAddDto articleAddDto, string createdByName);
+        Task<IResult> Update(ArticleUpdateDto articleUpdateDto, string modifiedByName);
+        Task<IResult> Delete(int articleId, string modifiedByName);
+        Task<IResult> HardDelete(int articleId);
+    }
+}
