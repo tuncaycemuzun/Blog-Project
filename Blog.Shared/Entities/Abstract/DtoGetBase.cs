8275d952ff115861bc296925c60f755b1f21be02
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-19 19:42:53 +0300

    Create DtoGetBase.cs

---
+++ Blog.Shared/Entities/Abstract/DtoGetBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Shared.Utilities.Results.ComplexTypes;
+
+namespace Blog.Shared.Entities.Abstract
+{
+    public abstract class DtoGetBase
+    {
+        public virtual ResultStatus ResultStatus { get; set; }
+    }
+}
