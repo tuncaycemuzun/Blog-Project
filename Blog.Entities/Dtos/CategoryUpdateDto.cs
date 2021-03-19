6cb0b63ffc2d703cb0e9499ee1bd73733dd46601
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-19 18:29:35 +0300

    Update CategoryUpdateDto.cs

--- Blog.Entities/Dtos/CategoryUpdateDto.cs
+++ Blog.Entities/Dtos/CategoryUpdateDto.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,27 @@ namespace Blog.Entities.Dtos
 {
     public class CategoryUpdateDto
     {
+        [Required]
+        public int Id { get; set; }
+
+        [DisplayName("Kategori Adı")]
+        [Required(ErrorMessage = "{0} Boş geçilmemelidir.")]
+        [MaxLength(70, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden küçük olmamalıdır.")]
+        public string Name { get; set; }
+
+        [DisplayName("Kategori Açıklaması")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden küçük olmamalıdır.")]
+        public string Description { get; set; }
+
+        [DisplayName("Kategori Özel Not Alanı")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden küçük olmamalıdır.")]
+        public string Note { get; set; }
+
+        [DisplayName("Aktif Mi?")]
+        [Required(ErrorMessage = "{0} Boş geçilmemelidir")]
+        public bool IsActive { get; set; }
     }
 }
