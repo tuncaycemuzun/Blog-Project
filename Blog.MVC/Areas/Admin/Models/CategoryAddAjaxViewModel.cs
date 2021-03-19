d975ab06b8c3541ca84c54eb7c0722d576b5ba2a
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-20 00:22:41 +0300

    Create CategoryAddAjaxViewModel.cs

---
+++ Blog.MVC/Areas/Admin/Models/CategoryAddAjaxViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Entities.Dtos;
+
+namespace Blog.MVC.Areas.Admin.Models
+{
+    public class CategoryAddAjaxViewModel
+    {
+        public CategoryAddDto CategoryAddDto { get; set; }
+        public string CategoryAddPartial { get; set; }
+        public CategoryDto CategoryDto { get; set; }
+
+    }
+}
