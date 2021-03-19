ca20a828334501adaed41900e46565068c031063
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-19 17:59:14 +0300

    Created Utilies folder and utilies items

---
+++ Blog.Shared/Utilities/Results/Abstract/IResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Shared.Utilities.Results.ComplexTypes;
+
+namespace Blog.Shared.Utilities.Results.Abstract
+{
+    public interface IResult
+    {
+        public ResultStatus ResultStatus { get;} //ResultStatus.Success or ResultStatus.Error
+        public string Message { get;}
+        public Exception Exception { get;}
+    }
+    
+}
