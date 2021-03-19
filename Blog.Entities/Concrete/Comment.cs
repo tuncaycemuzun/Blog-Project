2adc7a593e2d8757ba34bfee0a8ea902c117accf
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-19 14:49:08 +0300

    Update Comment.cs

--- Blog.Entities/Concrete/Comment.cs
+++ Blog.Entities/Concrete/Comment.cs
@@ -10,7 +10,7 @@ namespace Blog.Entities.Concrete
     public class Comment:EntityBase,IEntity
     {
         public string Text { get; set; }
-        public int ActicleId { get; set; }
+        public int ArticleId { get; set; }
         public Article Article { get; set; }
     }
 }
