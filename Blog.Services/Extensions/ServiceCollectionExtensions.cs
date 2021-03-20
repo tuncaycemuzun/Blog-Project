211472a01cb1ad55bf9a122af2d009221c95a52d
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-20 20:46:08 +0300

    Update ServiceCollectionExtensions.cs

--- Blog.Services/Extensions/ServiceCollectionExtensions.cs
+++ Blog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -18,7 +18,21 @@ namespace Blog.Services.Extensions
         public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollection)
         {
             serviceCollection.AddDbContext<BlogContext>();
-            serviceCollection.AddIdentity<User, Role>().AddEntityFrameworkStores<BlogContext>();
+            serviceCollection.AddIdentity<User, Role>(options=>
+            {
+                //Şifre Seçenekleri
+                options.Password.RequireDigit = false;//Şifrede rakam olması zorunlu mu
+                options.Password.RequiredLength = 5; //Zorunlu şifre uzunluğu
+                options.Password.RequiredUniqueChars = 0;//Unique karakterden en az kaç tane olması gerekiyor?
+                options.Password.RequireNonAlphanumeric = false;//Özel karakterler kullanılsın mı?
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+
+                //Kullanıcı Adı ve Email Seçenekleri
+                options.User.AllowedUserNameCharacters= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;
+
+            }).AddEntityFrameworkStores<BlogContext>();
             serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
             serviceCollection.AddScoped<ICategoryService, CategoryManager>();
             serviceCollection.AddScoped<IArticleService, ArticleManager>();
