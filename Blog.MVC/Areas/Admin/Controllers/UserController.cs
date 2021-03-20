eb5aa290011163a1e296ded291d1beda958ef4f7
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-20 21:35:50 +0300

    Create UserController.cs

---
+++ Blog.MVC/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Entities.Concrete;
+using Blog.Entities.Dtos;
+using Blog.Shared.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return View(new UserListDto
+            {
+                Users = users,
+                ResultStatus = ResultStatus.Success
+            });
+
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return PartialView("_UserAddPartial");
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Add(UserAddDto userAddDto)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+                
+        //    }
+        //}
+    }
+}
