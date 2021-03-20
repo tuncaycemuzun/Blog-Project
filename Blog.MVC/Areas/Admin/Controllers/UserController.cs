b7d10a648752f1a8b2dca720fdc7d53145a778c2
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-20 23:06:31 +0300

    added userAddDto

--- Blog.MVC/Areas/Admin/Controllers/UserController.cs
+++ Blog.MVC/Areas/Admin/Controllers/UserController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using AutoMapper;
 using Blog.Entities.Concrete;
 using Blog.Entities.Dtos;
+using Blog.MVC.Areas.Admin.Models;
+using Blog.Shared.Utilities.Extensions;
 using Blog.Shared.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.EntityFrameworkCore;
 
 namespace Blog.MVC.Areas.Admin.Controllers
@@ -15,10 +24,14 @@ namespace Blog.MVC.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly UserManager<User> _userManager;
+        private readonly IWebHostEnvironment _env;
+        private readonly IMapper _mapper;
 
-        public UserController(UserManager<User> userManager)
+        public UserController(UserManager<User> userManager, IWebHostEnvironment env, IMapper mapper)
         {
             _userManager = userManager;
+            _env = env;
+            _mapper = mapper;
         }
 
         public async Task<IActionResult> Index()
@@ -38,13 +51,81 @@ namespace Blog.MVC.Areas.Admin.Controllers
             return PartialView("_UserAddPartial");
         }
 
-        //[HttpPost]
-        //public async Task<IActionResult> Add(UserAddDto userAddDto)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-                
-        //    }
-        //}
+        [HttpPost]
+        public async Task<IActionResult> Add(UserAddDto userAddDto)
+        {
+            if (ModelState.IsValid)
+            {
+                userAddDto.Picture = await ImageUpload(userAddDto);
+                var user = _mapper.Map<User>(userAddDto);
+                var result = await _userManager.CreateAsync(user, userAddDto.Password);
+                if (result.Succeeded)
+                {
+                    var userAddAjaxModel = JsonSerializer.Serialize(new UserAddAjaxViewModel
+                    {
+                        UserDto = new UserDto
+                        {
+                            ResultStatus = ResultStatus.Success,
+                            Message = $"{user.UserName} adlı kullanıcı adına sahip kullanıcı başarıyla eklenmiştir.",
+                            User = user
+                        },
+                        UserAddPartial = await this.RenderViewToStringAsync("_UserAddPartial", userAddDto)
+                    });
+                    return Json(userAddAjaxModel);
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("",error.Description);
+                    }
+
+                    var useraddAjaxErrorModal = JsonSerializer.Serialize(new UserAddAjaxViewModel
+                    {
+                        UserAddDto = userAddDto,
+                        UserAddPartial = await this.RenderViewToStringAsync("_UserAddPartial", userAddDto)
+                    });
+                    return Json(useraddAjaxErrorModal);
+                }
+            }
+            var useraddAjaxModalStateErrorModal = JsonSerializer.Serialize(new UserAddAjaxViewModel
+            {
+                UserAddDto = userAddDto,
+                UserAddPartial = await this.RenderViewToStringAsync("_UserAddPartial", userAddDto)
+            });
+            return Json(useraddAjaxModalStateErrorModal);
+        }
+
+
+        
+        public async Task<string> ImageUpload(UserAddDto userAddDto)
+        {
+            string wwroot = _env.WebRootPath;
+            string fileExtension = Path.GetExtension(userAddDto.PictureFile.FileName);
+            DateTime dateTime = DateTime.Now;
+            string fileName = $"{userAddDto.Username}_{dateTime.FullDateAndTimeStringWithUnderscore()}{fileExtension}";
+            var path = Path.Combine($"{wwroot}/userImage",fileName);
+            await using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await userAddDto.PictureFile.CopyToAsync(stream);
+            }
+
+            return fileName;
+        }
+        [HttpGet]
+        public async Task<JsonResult> GetAllUsers()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var userListDto = JsonSerializer.Serialize(new UserListDto
+            {
+                Users = users,
+                ResultStatus = ResultStatus.Success
+            },
+            new JsonSerializerOptions
+            {
+                ReferenceHandler = ReferenceHandler.Preserve
+            });
+            return Json(userListDto);
+        }
     }
 }
---
+++ Blog.MVC/Areas/Admin/Models/UserAddAjaxViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Entities.Dtos;
+
+namespace Blog.MVC.Areas.Admin.Models
+{
+    public class UserAddAjaxViewModel
+    {
+        public UserAddDto UserAddDto { get; set; }
+        public string UserAddPartial { get; set; }
+        public UserDto UserDto { get; set; }
+
+    }
+}
