edf61f03a1c9b26a02ef45658d682d19981e82e0
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-20 20:13:23 +0300

    Editing Mapping

---
+++ Blog.Data/Concrete/EntityFramework/Mappings/RoleClaimMap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Data.Concrete.EntityFramework.Mappings
+{
+    public class RoleClaimMap : IEntityTypeConfiguration<RoleClaim>
+    {
+        public void Configure(EntityTypeBuilder<RoleClaim> builder)
+        {
+            // Primary key
+            builder.HasKey(rc => rc.Id);
+
+            // Maps to the AspNetRoleClaims table
+            builder.ToTable("AspNetRoleClaims");
+        }
+    }
+}
