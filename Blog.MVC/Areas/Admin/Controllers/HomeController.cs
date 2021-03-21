010ede0067cd8875ad4018a0b2abf5e8f8379b2c
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-21 14:20:32 +0300

    Update HomeController.cs

--- Blog.MVC/Areas/Admin/Controllers/HomeController.cs
+++ Blog.MVC/Areas/Admin/Controllers/HomeController.cs
@@ -3,10 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Blog.MVC.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
