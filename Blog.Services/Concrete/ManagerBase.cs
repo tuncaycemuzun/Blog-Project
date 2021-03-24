f2414b04612c69d5906e9eb9a27b7aa0b93d3039
tuncaycemuzun <tuncaycemuzun@outlook.com>
2021-03-24 23:39:41 +0300

    Create ManagerBase.cs

---
+++ Blog.Services/Concrete/ManagerBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Blog.Data.Abstract;
+
+namespace Blog.Services.Concrete
+{
+    public class ManagerBase 
+    {
+        protected IUnitOfWork UnitOfWork;
+        protected readonly IMapper Mapper;
+
+        public ManagerBase(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            UnitOfWork = unitOfWork;
+            Mapper = mapper;
+        }
+    }
+}
